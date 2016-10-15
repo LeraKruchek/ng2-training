60f14e4d37b428ee6932b5593e1a897558d5aaef
Valeryia_Kruchak <valeryia_kruchak@epam.com>
2016-10-15 18:45:12 +0300

    added examples for simple pipes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent }  from './app.component';
 import { PipesComponent }  from './pipes/pipes.component';
 
 @NgModule({
-  imports: [ BrowserModule,
+  imports: [ BrowserModule, FormsModule,
     RouterModule.forRoot([
       { path: 'pipes', component: PipesComponent },
       { path: '', redirectTo: '/pipes', pathMatch: 'full' }
--- app/pipes/pipes.component.ts
+++ app/pipes/pipes.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+// import { fromEvent } from 'rxjs/observable/fromEvent';
+// import { interval } from 'rxjs/observable/interval';
+
 
 @Component({
   selector: 'pipes',
-  templateUrl: 'app/pipes/pipes.component.html'
+  templateUrl: 'app/pipes/pipes.component.html',
+  styleUrls: ['app/pipes/pipes.component.css']
 })
-export class PipesComponent {
-  lowerCase: string;
+export class PipesComponent implements AfterViewInit {
+  lowerCase: string = 'Hi, Folks!';
+  upperCase: string = 'Hi, Folks!';
+  pi: number = 3.141592654;
+  percent: number = 0.141592654;
+  date: number = Date.now();
+  sliceString: string = '';
+
+  $start_click: Observable<any>;
+  $stop_click: Observable<any>;
+  $num_stream: Observable<any>;
+
+  ngAfterViewInit() {
+    this.$start_click = Observable.fromEvent(document.getElementById('start_btn'), 'click');
+    this.$stop_click = Observable.fromEvent(document.getElementById('stop_btn'), 'click');
+    this.$num_stream = this.$start_click.switchMap(() => {
+      return Observable.interval(500).takeUntil(this.$stop_click);
+    });
+  }
+  
+  constructor() {};
+
+  checkLength(e) {
+    if (e.currentTarget.value.length > 15) {
+      e.preventDefault();
+    }
+  }
 }
